95b2670650549170a0dc562ca36ccdc145842880
Dhruv <dhruv.nitj@gmail.com>
2024-05-07 12:01:59 +0530

    updated

--- app.py
+++ app.py
@@ -1,3 +1,48 @@
+# import streamlit as st
+# import numpy as np
+# import pandas as pd
+# from PIL import Image
+# from tensorflow.keras.models import load_model
+# from tensorflow.keras.preprocessing.image import img_to_array
+
+# from keras.layers import Softmax
+
+# model = load_model('model.h5')
+
+# st.title("Skin Cancer Prediction System")
+
+# uploaded = st.file_uploader("Upload Image", type=['jpg', 'jpeg', 'png'])
+
+# if uploaded is not None:
+#     image = Image.open(uploaded)
+#     st.image(image, caption="Input Cell Image", width=200)
+
+#     def preprocess_image(uploaded_image):
+#         resized_image = uploaded_image.resize((256, 256))
+#         image_array = img_to_array(resized_image)
+#         image_array /= 255.
+#         return image_array
+
+#     def prediction(image_array):
+#         pred = model.predict(np.expand_dims(image_array, axis=0))
+#         return pred
+
+#     inp = preprocess_image(image)
+#     ans = prediction(inp)
+#     classes = ['Benign', 'Malignant']
+
+#     pred_class = np.argmax(ans)
+#     confidence, pred_class_name = ans[0][pred_class], classes[pred_class]
+
+#     confidence_percentage = round(confidence * 100, 2)
+#     data = {
+#         'Predicted Class': [pred_class_name],
+#         'Confidence (%)': [confidence_percentage]
+#     }
+#     df = pd.DataFrame(data)
+#     st.write(df)
+
+
 import streamlit as st
 import numpy as np
 import pandas as pd
@@ -5,39 +50,38 @@ from PIL import Image
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
 
-from keras.layers import Softmax
-
 model = load_model('model.h5')
 
 st.title("Skin Cancer Prediction System")
 
-uploaded = st.file_uploader("Upload Image", type=['jpg', 'jpeg', 'png'])
+uploaded_files = st.file_uploader("Upload Image", type=['jpg', 'jpeg', 'png'], accept_multiple_files=True)
 
-if uploaded is not None:
-    image = Image.open(uploaded)
-    st.image(image, caption="Input Cell Image", width=200)
+if uploaded_files:
+    for uploaded_file in uploaded_files:
+        image = Image.open(uploaded_file)
+        st.image(image, caption="Input Cell Image", width=200)
 
-    def preprocess_image(uploaded_image):
-        resized_image = uploaded_image.resize((256, 256))
-        image_array = img_to_array(resized_image)
-        image_array /= 255.
-        return image_array
+        def preprocess_image(uploaded_image):
+            resized_image = uploaded_image.resize((256, 256))
+            image_array = img_to_array(resized_image)
+            image_array /= 255.
+            return image_array
 
-    def prediction(image_array):
-        pred = model.predict(np.expand_dims(image_array, axis=0))
-        return pred
+        def prediction(image_array):
+            pred = model.predict(np.expand_dims(image_array, axis=0))
+            return pred
 
-    inp = preprocess_image(image)
-    ans = prediction(inp)
-    classes = ['Benign', 'Malignant']
+        inp = preprocess_image(image)
+        ans = prediction(inp)
+        classes = ['Benign', 'Malignant']
 
-    pred_class = np.argmax(ans)
-    confidence, pred_class_name = ans[0][pred_class], classes[pred_class]
+        pred_class = np.argmax(ans)
+        confidence, pred_class_name = ans[0][pred_class], classes[pred_class]
 
-    confidence_percentage = round(confidence * 100, 2)
-    data = {
-        'Predicted Class': [pred_class_name],
-        'Confidence (%)': [confidence_percentage]
-    }
-    df = pd.DataFrame(data)
-    st.write(df)
+        confidence_percentage = round(confidence * 100, 2)
+        data = {
+            'Predicted Class': [pred_class_name],
+            'Confidence (%)': [confidence_percentage]
+        }
+        df = pd.DataFrame(data)
+        st.write(df)
