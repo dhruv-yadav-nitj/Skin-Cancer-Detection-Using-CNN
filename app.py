49d7f56db394e7361dde07910d5f542efa1da344
Dhruv <dhruv.nitj@gmail.com>
2024-05-07 10:54:14 +0530

    update

--- app.py
+++ app.py
@@ -4,6 +4,15 @@ from PIL import Image
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
 
+from keras.layers import Softmax
+
+# Define custom objects with Softmax activation function
+custom_objects = {'Softmax': Softmax}
+
+# Load the model with custom objects
+model = load_model('modelv1.h5', custom_objects=custom_objects)
+
+
 st.title("Skin Cancer Prediction System")
 
 uploaded = st.file_uploader("Upload Image", type=['jpg', 'jpeg', 'png'])
@@ -11,8 +20,6 @@ uploaded = st.file_uploader("Upload Image", type=['jpg', 'jpeg', 'png'])
 if uploaded is not None:
     image = Image.open(uploaded)
     st.image(image, caption="Input Cell Image", width=200)
-
-    model = load_model('modelv1.h5')
 
     def preprocess_image(uploaded_image):
         resized_image = uploaded_image.resize((256, 256))
